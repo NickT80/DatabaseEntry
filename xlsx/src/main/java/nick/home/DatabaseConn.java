ebe323038ee7ca4aeb6561ae9cb63c35e992de56
Nick Taylor <ntaylor800@gmail.com>
2017-01-08 19:59:28 -0700

    new project

---
+++ xlsx/src/main/java/nick/home/DatabaseConn.java
@@ -0,0 +1,47 @@
+package nick.home;
+
+/**
+ * Created by Nick Taylor on 12/31/2016.
+ */
+
+public class DatabaseConn {
+    /*Connection conn = null;
+
+
+    public void OpenDatabase() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager
+                    .getConnection("jdbc:postgresql://192.168.1.224:5432/postgres",
+                            "postgres", "");
+            conn.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void CloseDatabase() {
+        try {
+            System.out.println("Attempting to close");
+            conn.close();
+            System.out.println("Closed Connection to DB");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public void Commit(){
+        try{
+            System.out.println("Attempting to commit");
+            conn.commit();
+            System.out.println("Committed");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }*/
+}
+
---
+++ xlsx/src/main/java/nick/home/Main.java
@@ -0,0 +1,97 @@
+package nick.home;
+
+import nick.POI.ReadXLSX;
+
+import java.io.IOException;
+import java.sql.*;
+
+/**
+ * Created by Nick Taylor on 12/30/2016.
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, IOException {
+        Connection conn = null;
+        Statement stmt = null;
+        //establish connection
+
+        ReadXLSX xl = new ReadXLSX();
+        System.out.println(xl.toString());
+
+        /*try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager
+                    .getConnection("jdbc:postgresql://192.168.1.224:5432/postgres",
+                            "postgres", "");
+            conn.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        stmt = conn.createStatement();
+
+        System.out.println("Attempting to create a statement.");
+        *//*try {
+            String sql = "CREATE TABLE CALLS" +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " LENGTH         INT     NOT NULL," +
+                    " NUMBER         REAL     NOT NULL)";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }*//*
+
+        System.out.println("Attempting to add data.");
+        String sql = "INSERT INTO PEOPLE (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (6,'Larry', 46, 'Colorado', '5600000')";
+        stmt.executeUpdate(sql);
+
+        sql = "INSERT INTO CALLS (ID, NAME, LENGTH, NUMBER) "
+                + "VALUES (4,'Nick', '5', '4543333333')";
+        stmt.executeUpdate(sql);
+
+        ResultSet rs = stmt.executeQuery( "SELECT * FROM PEOPLE;" );
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  name = rs.getString("name");
+            int age  = rs.getInt("age");
+            String  address = rs.getString("address");
+            float salary = rs.getFloat("salary");
+            System.out.println( "ID = " + id );
+            System.out.println( "NAME = " + name );
+            System.out.println( "AGE = " + age );
+            System.out.println( "ADDRESS = " + address );
+            System.out.println( "SALARY = " + salary );
+            System.out.println();
+        }
+
+        rs = stmt.executeQuery( "SELECT * FROM CALLS;" );
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  name = rs.getString("name");
+            int length  = rs.getInt("length");
+            long number = rs.getLong("number");
+            System.out.println( "ID = " + id );
+            System.out.println( "NAME = " + name );
+            System.out.println( "LENGTH in minutes = " + length );
+            System.out.println( "NUMBER = " + number);
+            System.out.println();
+        }
+
+        rs.close();
+        stmt.close();
+        conn.commit();
+        conn.close();*/
+    }
+
+   /* public void addPeople(int ID, String NAME, int AGE, int ADDRESS, int SALARY) throws SQLException {
+        System.out.println("Attempting to add data.");
+        String sql = "INSERT INTO PEOPLE (ID,NAME,AGE,ADDRESS,SALARY) "
+                + "VALUES (1,'John', 32, 'California', '100000');";
+        stmt.executeUpdate(sql);
+    }*/
+}
